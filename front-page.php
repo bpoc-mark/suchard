fad5478618be499b8ec6a0253e97c07796b8bce6
kirk-patrik <kirk_patrik@bpoc.co.jp>
2021-07-15 17:46:19 +0800

    sect_1 pc&sp

--- front-page.php
+++ front-page.php
@@ -1,5 +1,76 @@
 <?php get_header();?>
-    <section>1</section>
+    <section class="sect_1">
+        <div class="sect_1__wrapper">
+            <div class="l-wrap">
+
+                    <!-- Heading -->
+                    <p class="heading_title">NEWS</p>
+
+                    <!-- Four circle -->
+                    <div class="sect_1__circle">
+                        <span class="circle-1"></span>
+                        <span class="circle-1"></span>
+                        <span class="circle-1"></span>
+                        <span class="circle-1"></span>
+                    </div>
+
+                    <!-- contents -->
+                    <div class="content">
+                        <!-- first content -->
+                        <div class="cont_1">
+
+                            <div class="col_1">
+                                <p class="number">2021.00.00</p>
+                                <span class="barrier"></span>
+                                <p class="para_1">ダミーテキストダミーテキスト...</p>
+                            </div>
+
+                            <div class="col_2">
+                                <p class="read_more">READ MORE</p>
+                                <img src="<?php echo get_template_directory_uri(); ?>/release/image/sect_1/arrow_icon.png" alt="">
+                            </div>
+
+                        </div>
+
+                        <!-- second content -->
+                        <div class="cont_1">
+
+                            <div class="col_1">
+                                <p class="number">2021.00.00</p>
+                                <span class="barrier"></span>
+                                <p class="para_1">ダミーテキストダミーテキスト...</p>
+                            </div>
+
+                            <div class="col_2">
+                                <p class="read_more">READ MORE</p>
+                                <img src="<?php echo get_template_directory_uri(); ?>/release/image/sect_1/arrow_icon.png" alt="">
+                            </div>
+
+                        </div>
+
+                        <!-- third content -->
+                        <div class="cont_1">
+
+                            <div class="col_1">
+                                <p class="number">2021.00.00</p>
+                                <span class="barrier"></span>
+                                <p class="para_1">ダミーテキストダミーテキスト...</p>
+                            </div>
+
+                            <div class="col_2">
+                                <p class="read_more">READ MORE</p>
+                                <img src="<?php echo get_template_directory_uri(); ?>/release/image/sect_1/arrow_icon.png" alt="">
+                            </div>
+
+                        </div>
+                    </div>
+                    
+            </div>
+        </div>
+    </section>
+
+
+
     <section>2</section>
     <section>3</section>
     <section>4</section>
