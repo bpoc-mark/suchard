788d1589f584e9d8e7052722d27608284dd974d7
arsua-greg <gregarsua@bpoc.co.jp>
2021-07-16 13:27:48 +0800

    finish with section 3 pc and sp

--- front-page.php
+++ front-page.php
@@ -1,6 +1,62 @@
-<?php get_header();?>
-    <section>1</section>
-    <section>2</section>
-    <section>3</section>
-    <section>4</section>
-<?php get_footer();?>
+<?php get_header(); ?>
+<section>1</section>
+<section>2</section>
+<section class="sect_3">
+    <div class="sect_3__wrapper">
+        <div class="l-wrap">
+
+            <div class="sect_3__takeout">
+                <img src="<?php echo get_template_directory_uri(); ?>/release/image/sect_3/take_out.png" alt="">
+            </div>
+
+            <div class="sect_3__pack">
+
+                <div class="col-1">
+                    <img src="<?php echo get_template_directory_uri(); ?>/release/image/sect_3/pack_1.png" alt="">
+                    <img src="<?php echo get_template_directory_uri(); ?>/release/image/sect_3/pack_2.png" alt="">
+                    <img src="<?php echo get_template_directory_uri(); ?>/release/image/sect_3/pack_2.png" alt="">
+                </div>
+
+                <div class="col-2">
+                    <p class="title">お弁当ご予約受付中</p>
+                    <img src="<?php echo get_template_directory_uri(); ?>/release/image/sect_3/pack_1.png" alt="">
+                    <p class="desc">予約受付時間 当日10:00まで<br>／お渡し時間11:30〜／お弁当ラストオーダー16:00まで</p>
+                    <p class="txt">当店自慢の味をテイクアウトで。大きな鮭がどんと乗ったスペシャル弁当とワンコイン弁当をご用意しております。</p>
+                    <table>
+                        <tr>
+                            <td>スペシャル弁当</td>
+                            <td>¥700</td>
+                        </tr>
+                        <tr>
+                            <td>焼き鮭弁当</td>
+                            <td>¥500</td>
+                        </tr>
+                        <tr>
+                            <td>唐揚げ弁当</td>
+                            <td>¥500</td>
+                        </tr>
+                        <tr>
+                            <td>焼肉弁当</td>
+                            <td>¥500</td>
+                        </tr>
+                    </table>
+
+                    <img src="<?php echo get_template_directory_uri(); ?>/release/image/sect_3/pack_2.png" alt="">
+                    <img src="<?php echo get_template_directory_uri(); ?>/release/image/sect_3/pack_2.png" alt="">
+                </div>
+            </div>
+            <div class="sect_3__contact_info">
+                <div class="line">
+                    <img src="<?php echo get_template_directory_uri(); ?>/release/image/sect_3/line_icon.png" alt="">
+                    <p class="txt">LINEで注文</p>
+                </div>
+
+                <div class="phone">
+                    <img src="<?php echo get_template_directory_uri(); ?>/release/image/sect_3/phone.png" alt="">
+                    <p class="txt">電話で注文</p>
+                </div>
+            </div>
+        </div>
+</section>
+<section>4</section>
+<?php get_footer(); ?>
