73929594ee568a3f5a7ac5b64868c37a72116b13
bpoc-mark <markariel.maata@gmail.com>
2021-07-19 10:48:36 +0800

    added pc and sp view on header

--- header.php
+++ header.php
@@ -20,6 +20,45 @@
     
 <main>
     <div class="page__header">
-        header
+        <div class="page__wrapper">
+            <div class="head_cont">
+                <div class="l-wrap">
+                    <p class="txt_info">スシャールへ<br>ようこそ。</p>
+                    <ul class="head_dot">
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                    <h2 class="txt_head">
+                        挽きたての珈琲の<br>
+                        香りに包まれる<br>
+                        とっておきの時間
+                    </h2>
+                    <ul class="head_dot">
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                    <div class="head_img_3">
+                    <img src="<?php echo get_template_directory_uri();?>/release/image/header/header_img_3.png" alt="">
+                </div>
+                </div>
+                <div class="head_img_1">
+                    <img src="<?php echo get_template_directory_uri();?>/release/image/header/header_img_1.png" alt="">
+                </div>
+                <div class="head_img_2">
+                    <img src="<?php echo get_template_directory_uri();?>/release/image/header/header_img_2.png" alt="">
+                </div>
+                <a class="ig" href="https://www.instagram.com/suchard0963/?hl=ja" target="_blank">
+                    <img src="<?php echo get_template_directory_uri();?>/release/image/header/ig.png" alt="">
+                </a>
+                <div class="line1"></div>
+                <div class="line2"></div>
+                <div class="line3"></div>
+                <div class="line4"></div>
+            </div>
+        </div>
     </div>
     <div class="page__content">
